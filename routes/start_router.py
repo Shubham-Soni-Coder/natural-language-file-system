23c37ee292038c47140e9af139265aebb8e4a901
TESTER-Repo <nv625590@gmail.com>
2026-07-25 18:11:49 +0000

    Fix the hardcoded value of folder path

--- routes/start_router.py
+++ routes/start_router.py
@@ -1,8 +1,7 @@
-from utils import SessionLocal
+from utils import SessionLocal,settings
 from services import FileService
 from core import FileUtils
 from fastapi import APIRouter
-from pathlib import Path
 from utils import main_logger as logger
 
 route = APIRouter()
@@ -10,7 +9,7 @@ route = APIRouter()
 @route.on_event("startup")
 def startup_event():
     db = SessionLocal()
-    foldername = "/home/ubuntu/natural-language-file-system"
+    foldername = settings.FOLDER_PATH
     scanner = FileUtils(foldername)
     try:
         generator = scanner.scan(include_hash=True)
